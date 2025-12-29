f1896aadf69e4929e4b5f7ee03acbf0283c9fcca
manuNamuncura <manu.namun94@gmail.com>
2025-12-29 12:55:20 -0300

    nuevos cambios

--- src/main/java/com/gestion/getionempresarial/GetionEmpresarialApplication.java
+++ src/main/java/com/gestion/getionempresarial/GetionEmpresarialApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GetionEmpresarialApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GetionEmpresarialApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GetionEmpresarialApplication.class, args);
+    }
 
 }
--- src/test/java/com/gestion/getionempresarial/GetionEmpresarialApplicationTests.java
+++ src/test/java/com/gestion/getionempresarial/GetionEmpresarialApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GetionEmpresarialApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
