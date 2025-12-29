eb3e33203a3bf919c04c8f1c758870cdd76af43a
manuNamuncura <manu.namun94@gmail.com>
2025-12-29 12:53:39 -0300

    first commit

---
+++ src/main/java/com/gestion/getionempresarial/GetionEmpresarialApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.getionempresarial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetionEmpresarialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetionEmpresarialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestion/getionempresarial/GetionEmpresarialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.getionempresarial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetionEmpresarialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
